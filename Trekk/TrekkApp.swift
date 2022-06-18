97b1408a59ea5425ef456092755dc705134c63d5
Tom Riley <tom.riley@outlook.com>
2022-06-18 19:37:05 +0100

    Initial Commit

---
+++ Trekk/TrekkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekkApp.swift
+//  Trekk
+//
+//  Created by Tom Riley on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrekkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
